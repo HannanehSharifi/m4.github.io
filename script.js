1961806b66278c9d7d3d6fa3dbbe4d43f166bf58
HannanehSharifi <hannaneh.sharifii@gmail.com>
2024-11-07 21:07:57 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (let i = 0; i < names.length; i++) {
+  const name = names[i];
+
+  if (name.charAt(0).toLowerCase() === 'j') {
+    console.log("Goodbye" + name);
+  } else {
+    console.log("Hello" + name);
+  }
+}
+
+
